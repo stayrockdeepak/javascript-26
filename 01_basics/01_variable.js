4b1812450fb4e21da54f1225195be33a4f8bbff0
Deepak Sharma <42131548+stayrockdeepak@users.noreply.github.com>
2026-01-05 17:42:42 +0000

    feat: variables explained

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountId = 12345;
+let accountName = 'Dummy';
+var accountPw = '6789';
+accountCity = 'Bengaluru';
+
+console.table([accountId,accountName,accountPw,accountCity]); // This will print the result in tabular format
+
